5c26ea91e4e24fe7995452cbbb4e71fdb8f5b898
rinoux <rinoux@foxmail.com>
2017-08-07 14:41:35 +0800

    no message

--- src/main/java/cc/rinoux/A.java
+++ src/main/java/cc/rinoux/A.java
@@ -5,39 +5,20 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * Created by rinoux on 2017/7/13.
  */
-public class A {
+public final class A {
 
-    final static A a = new A();
+
+    private static String s;
 
     static {
-        System.out.println(a);
+        System.out.println(A.class.getClassLoader());
+        s = "static";
+        System.out.println(s);
     }
 
 
     public static void main(String[] args) throws Exception {
-
-        final ReentrantLock lock = new ReentrantLock();
-
-
-        Thread t = new Thread(new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    lock.lock();
-                    System.out.println("execute");
-                    lock.unlock();
-                }
-            });
-
-            lock.lock();
-            lock.lock();
-            t.start();
-
-            Thread.sleep(200);
-            System.out.println("realse one once");
-
-            lock.unlock();
-        }
-
+        System.out.println(A.class.getClassLoader());
+        System.out.println("main");
+    }
 }
